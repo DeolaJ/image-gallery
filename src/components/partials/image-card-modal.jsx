4a9cf24f4361977a855da542257d4456ffab8a48
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2021-01-09 11:15:49 +0100

    IMG-11 | Fixed lazy loading issue on modal
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- src/components/partials/image-card-modal.jsx
+++ src/components/partials/image-card-modal.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable max-lines-per-function */
 import { useRef } from 'react';
 import PropTypes from 'prop-types';
-import LazyLoad from 'react-lazyload';
 import Fade from 'react-reveal/Fade';
 
 import ModalWrapper from './modal-wrapper';
@@ -20,17 +19,15 @@ const ImageCardModal = ({
     <ModalWrapper closeModal={closeModal}>
       <div className="image-modal">
         <div className="image-modal__placeholder" ref={refPlaceholder} />
-        <LazyLoad once>
-          <Fade duration={500}>
-            <img
-              className="image-modal__loaded-image"
-              onLoad={removePlaceholder}
-              onError={removePlaceholder}
-              src={image.urls.regular}
-              alt={image.alt}
-            />
-          </Fade>
-        </LazyLoad>
+        <Fade duration={500}>
+          <img
+            className="image-modal__loaded-image"
+            onLoad={removePlaceholder}
+            onError={removePlaceholder}
+            src={image.urls.regular}
+            alt={image.alt}
+          />
+        </Fade>
         <div className="image-modal__content">
           <h4>
             {image.user.name}
